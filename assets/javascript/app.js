a88291342b99d163da7f473178c299154b2a2eba
The-Ryan-Mobley <49420591+The-Ryan-Mobley@users.noreply.github.com>
2019-07-17 21:13:36 -0500

    need to add timers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
     var total_questions =0;
     var wrong_questions=0;
     var completion = false;
+    var right_answer = "";
     var question_count=$("#question-count");
     var question_bilboard=$("#question-display");
     var response_display=$("#response-display");
@@ -26,65 +27,87 @@ $(document).ready(function(){
     
 
     function make_answers(index){ ///needs to splice after answer generation
-        
         for(let i=0;i<4;i++){
             let answer_space = $('<p class="answer-space">');
             let answer_text = responses[index][i];
-            answer_space.attr('id',i);
             answer_space.appendTo(response_display);
             answer_space.html(answer_text);
             answer_space.data("text-data",answer_text);
             
-
         }
-        responses.splice(index,1);
+        
         
         
         
     }
     function make_question(){
-        let rand = Math.floor(Math.random()*questions.length); //sets up random number from questions array
         
-        question_to_display = set_question_to_display(rand); //sets value for the current question
-
-        make_answers(rand);
-        question_count.html("Question #" + total_questions++ + ":");
+        
+        question_to_display = questions[total_questions];
+        find_right_answer();
+        make_answers(total_questions);
+        question_count.html("Question #" + total_questions + ":");
         question_bilboard.html(question_to_display);
-        
-        
-        
-        
-
-
-
-
+        total_questions++;
     }
-    function set_question_to_display(rand){
-       
-        let the_question = questions[rand];
-        
-        if(questions.length > 1){
-            questions.splice(questions.indexOf(the_question),1);
-
-        }
-        else{
-          completion = true;  
-        }
-        
-        return the_question;
-
-    }
+    
     function clean_answers(){
         response_display.empty();
     }
     function game_over(){ ////will eventually bring up results screen but for now just refreshes page
-        alert("GAME OVER");
         window.location.reload();
 
     }
-    function find_right_answer(){
-        let the_question = question_bilboard.get('text');
-        console.log(the_question);
+    function find_right_answer(TQ){
+        switch(TQ){
+            case 0:{
+                right_answer =responses[0][3];
+                break;
+            }
+            case 1:{
+                right_answer = responses[1][1];
+                break;
+            }
+            case 2:{
+                right_answer = responses[2][1];
+                break;
+            }
+            case 3:{
+                right_answer = responses[3][2];
+                break;
+            }
+            case 4:{
+                right_answer = responses[4][0];
+                break;
+            }
+
+        }
+    }
+    function user_answer(element){
+        console.log(element.data("text-data"));
+        if(element.data("text-data") !== right_answer){
+            wrong_questions++;
+            console.log(wrong_questions);
+        }
+
+
+    }
+    function endscreen(){
+        let answer_space = $('<p class="answer-space">');
+        let right_answers = total_questions - wrong_questions;
+        clean_answers();
+        question_count.html("GAME OVER!");
+        question_bilboard.html("YOUR SCORE: <br> Right Answers" + right_answers + "<br> Wrong Answers: " + wrong_questions + "<br> Final score: "+ right_answers*10);
+        answer_space.appendTo(response_display);
+        answer_space.html("Restart?");
+        
+
+    }
+    function check_state(){
+        if(total_questions === responses.length){
+            completion = true;
+            endscreen();
+        }
     }
 
 
@@ -93,10 +116,15 @@ $(document).ready(function(){
     make_question();
     //find_right_answer();
     
-    response_display.on("click", ".answer-space", function(){
+    response_display.on("click", ".answer-space", function(event){
         if(completion === false){
+            
+            user_answer(response_display.find(event.target));
             clean_answers();
             make_question();
+            check_state();
+        
+
         }
         else{
             game_over();
